921e774a7d87773821565ee400bd2b8d420bdf5b
jaymin-developer <kjmkjm822@naver.com>
2022-06-14 17:05:10 +0900

    project

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <h1>
+        <NuxtLink to="/">Nuxt Shopping</NuxtLink>
+      </h1>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route)
+  },
+}
+</script>
+
+<style></style>
